f2fc9a06ee2e48df8e52975ec8dc0e7ef53f7868
hardcoregor <spraytown2014@gmail.com>
2023-01-05 00:54:05 +0200

    navbar 100%, sidebar 100%, server developing

---
+++ client/src/utils/navArray.js
@@ -0,0 +1,77 @@
+import {
+  SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
+  PublicOutlined,
+  PointOfSaleOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
+  TrendingUpOutlined,
+  PieChartOutlined,
+  TodayOutlined
+} from "@mui/icons-material";
+
+const navItems = [
+  {
+    text: "Dashboard",
+    icon: <HomeOutlined />
+  },
+  {
+    text: "Client Facing",
+    icon: null,
+  },
+  {
+    text: "Products",
+    icon: <ShoppingCartOutlined />
+  },
+  {
+    text: "Custumers",
+    icon: <Groups2Outlined />
+  },
+  {
+    text: "Transactions",
+    icon: <ReceiptLongOutlined />
+  },
+  {
+    text: "Geography",
+    icon: <PublicOutlined />
+  },
+  {
+    text: "Sales",
+    icon: null
+  },
+  {
+    text: "Overview",
+    icon: <PointOfSaleOutlined />
+  },
+  {
+    text: "Daily",
+    icon: <TodayOutlined />
+  },
+  {
+    text: "Monthly",
+    icon: <CalendarMonthOutlined />
+  },
+  {
+    text: "Breakdown",
+    icon: <PieChartOutlined />
+  },
+  {
+    text: "Management",
+    icon: null
+  },
+  {
+    text: "Admin",
+    icon: <AdminPanelSettingsOutlined />
+  },
+  {
+    text: "Performance",
+    icon: <TrendingUpOutlined />
+  },
+]
+
+export default navItems;
---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
+
+//CONFIGURATION
+dotenv.config();
+const app = express();
+app.use(express.json()); //CONVERT BODY TO JSON
+app.use(helmet()); //PROTECT OUR APP
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors()); //ALLOW ALL TYPES OF REQUESTS GET,PUT,DELETE etc..
+
+//ROUTES
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use('/sales', salesRoutes);
+
+//MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server PORT: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
